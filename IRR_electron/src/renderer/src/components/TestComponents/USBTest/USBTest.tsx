2ca9a14c420c37d433f539ac85291417c8a2a3ca
Edgar Andree Rebollo Gomez <edgara.rebollo@reconext.com>
2024-05-24 14:46:42 -0700

    something

--- IRR_electron/src/renderer/src/components/TestComponents/USBTest/USBTest.tsx
+++ IRR_electron/src/renderer/src/components/TestComponents/USBTest/USBTest.tsx
@@ -2,21 +2,19 @@ import BaseLayout from '../../ui/baseLayout'
 import { Card, CardBody, Spinner, Progress } from '@nextui-org/react'
 import { useQuery } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
-import { executeUSBTest } from './services/usb'
+import { executeUSBTest, deleteUSBtestFile } from './services/usb'
 import { errorData } from '../../../utils/functions'
 import { IRR_MB_API } from '@renderer/utils/serviceEndPoints'
+import { ModalUSB } from '@renderer/components/ui'
+import { useDisclosure } from '@nextui-org/react'
 
-function USBTest({TestName, nextTest, profile}) {
+function USBTest({ TestName, nextTest, profile }) {
   const [progressValue, setProgressValue] = useState(0)
   const [downLoadComplete, setDownLoadComplete] = useState(false)
   const [downloadError, setDownloadError] = useState(false)
-  /* TODO
-    .-Cambiar UI conforme comienza la descarga y e inicia el test
-    .- Iniciar el test de usb una vez la descarga haya comenzado
-    .- Configurar el backend de python para que busque el archivo
-    .- al acabar el test ir al siguiente.
-  */
 
+
+  const { isOpen, onOpenChange } = useDisclosure()
 
   const { isLoading, isSuccess, error } = useQuery({
     queryKey: ['usbTest'],
@@ -26,8 +24,9 @@ function USBTest({TestName, nextTest, profile}) {
     refetchOnWindowFocus: false
   })
 
+
   const getTestFile = () => {
-    window.api.send('downLoadFile', { payload: { url: `${IRR_MB_API}/downloadTestFile/`} })
+    window.api.send('downLoadFile', { payload: { url: `${IRR_MB_API}/downloadTestFile/` } })
     // @ts-ignore
     window.api.on('update-download-progress', (event, progress) => {
       setProgressValue(progress.progress)
@@ -40,9 +39,9 @@ function USBTest({TestName, nextTest, profile}) {
     })
     // @ts-ignore
     window.api.on('download-error', (event, error) => {
-      if(error){
+      if (error) {
         setDownloadError(true)
-        nextTest(TestName, {result: false, message: "Hubo un fallo al tratar de descargar el archivo de prueba"})
+        nextTest(TestName, { result: false, message: "Hubo un fallo al tratar de descargar el archivo de prueba" })
       }
     })
   }
@@ -51,13 +50,27 @@ function USBTest({TestName, nextTest, profile}) {
     getTestFile()
   }, [])
 
-  useEffect(() => {
-    if(isSuccess) {
+  const makeTestAgain = () => {
+    executeUSBTest(profile.usb).then((response) => {
+      deleteUSBtestFile()
       nextTest(TestName, {
         result: true,
-        message: 'Prueba de USB exitosa'
+        message: "Prueba de USB exitosa"
       })
+    }).catch((error) => {
+      deleteUSBtestFile()
+      nextTest(TestName, {
+        result: false,
+        message: errorData(error)
+      })
+    })
+  }
+
+  useEffect(() => {
+    if (isSuccess) {
+      onOpenChange()
     } else if (!isLoading && error) {
+      deleteUSBtestFile()
       nextTest(TestName, {
         result: false,
         message: errorData(error)
@@ -67,22 +80,25 @@ function USBTest({TestName, nextTest, profile}) {
 
   return (
     <BaseLayout>
-      <Card className="p-10">
-        <CardBody>
-          {!downLoadComplete ? <div className="flex flex-col items-center gap-4">
-            <p>Iniciando descarga de archivo de pruebas</p>
-            <Progress
-              aria-label="Cargando..."
-              showValueLabel={true}
-              value={progressValue}
-              className="max-w-md"
-            />
-          </div> : <div className='flex items-center gap-4'>
-          <p>Ejecutando prueba de USB</p>
-          <Spinner color='primary'/>
-          </div>}
-        </CardBody>
-      </Card>
+      <>
+        <Card className="p-10">
+          <CardBody>
+            {!downLoadComplete ? <div className="flex flex-col items-center gap-4">
+              <p>Iniciando descarga de archivo de pruebas</p>
+              <Progress
+                aria-label="Cargando..."
+                showValueLabel={true}
+                value={progressValue}
+                className="max-w-md"
+              />
+            </div> : <div className='flex items-center gap-4'>
+              <p>Ejecutando prueba de USB</p>
+              <Spinner color='primary' />
+            </div>}
+          </CardBody>
+        </Card>
+        <ModalUSB isOpen={isOpen} onOpenChange={onOpenChange} action={makeTestAgain} />
+      </>
     </BaseLayout>
   )
 }
--- IRR_electron/src/renderer/src/components/ui/ModalUsb.tsx
+++ IRR_electron/src/renderer/src/components/ui/ModalUsb.tsx
@@ -1,6 +1,8 @@
 import { Modal, ModalContent, ModalHeader, ModalBody, Button } from '@nextui-org/react'
 import motionV1 from '../../utils/motionVariants'
 import { FaCheckCircle } from 'react-icons/fa'
+import { CiUsb } from "react-icons/ci";
+import { FaArrowRotateLeft } from "react-icons/fa6";
 
 function ModalAudioTestF({ isOpen, onOpenChange, action }) {
     return (
@@ -19,10 +21,14 @@ function ModalAudioTestF({ isOpen, onOpenChange, action }) {
                             <ModalHeader className="flex flex-col gap-2 text-3xl font-semibold">
                                 Se requiere segunda prueba
                             </ModalHeader>
+                            <div className='flex gap-2 items-center justify-center'>
+                                <CiUsb size={40} />
+                                <FaArrowRotateLeft size={25} />
+                            </div>
                             <ModalBody>
                                 <p>
                                     Para terminar la prueba, es necesario que desconecte todos los USB tipo C, los gire y los vuelva a conectar.
-                                    despues de conectarlo espere unos segundos, y presione el boton de listo.
+                                    despues de conectarlos espere unos segundos, y presione el boton de repetir prueba.
                                 </p>
                                 <Button
                                     className="bg-primary-500 text-white"
