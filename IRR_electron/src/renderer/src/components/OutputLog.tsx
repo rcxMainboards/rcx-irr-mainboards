ad06f276f924388192648fcf2ef5f17dd821b215
rcxMainboards <irrmainboards@gmail.com>
2024-06-25 14:55:50 -0700

    wifi trys added, need to fix uploading logs loading

--- IRR_electron/src/renderer/src/components/OutputLog.tsx
+++ IRR_electron/src/renderer/src/components/OutputLog.tsx
@@ -1,6 +1,6 @@
 import { OutputTable, ExitButton } from '../components/ui/index'
 import BaseLayout from './ui/baseLayout'
-import { Card, CardHeader } from '@nextui-org/react'
+import { Card, CardHeader, Spinner } from '@nextui-org/react'
 import { TbReportSearch } from 'react-icons/tb'
 import { useQuery, useMutation } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
@@ -10,6 +10,7 @@ import clsx from 'clsx'
 
 function OutputLog({ Results, user }) {
   const [message, setMessage] = useState('')
+  const [isPassed, setIsPassed] = useState()
 
   const { isLoading, data } = useQuery({
     queryKey: ['mbProfile'],
@@ -20,8 +21,7 @@ function OutputLog({ Results, user }) {
 
   const { mutate, isPending, isSuccess, isError } = useMutation({
     // subir los datos de la placa con junto con los resultados de las pruebas
-    mutationFn: (args: { tests: any; Passed: any; mainboard: any; user: string, mb_test_type: string }) =>
-      sendOutputLog(args),
+    mutationFn: (args: { tests: any; Passed: any; mainboard: any; user: string, mb_test_type: string }) => sendOutputLog(args),
     onSuccess: (data) => {
       setMessage(data.message)
     },
@@ -32,12 +32,13 @@ function OutputLog({ Results, user }) {
           'Ocurrio un error al tratar de subir los resultados de la pruebas, puede ser que la conexion con el servidor fallo o que el servidor no este disponible, comuniquese con ingenieria.'
         )
       }
-    }
+    },
   })
 
   useEffect(() => {
     if (!isLoading && data) {
       const isPassed = Results.every((test) => test.details.result)
+      setIsPassed(isPassed)
       const mainboardProfile = data
       const {product, sku} = mainboardProfile
       const newProduct = `${product}&${sku}`
@@ -47,7 +48,7 @@ function OutputLog({ Results, user }) {
       mutate({
         tests: Results,
         Passed: isPassed,
-        mainboard: {...mainboardProfile, product: newProduct}, //Cambiar
+        mainboard: {...mainboardProfile, product: newProduct, serial_number: mainboardProfile.SerialNumber_hp}, //Cambiar
         user: user,
         mb_test_type: 'PCaaS'
       })
@@ -57,7 +58,7 @@ function OutputLog({ Results, user }) {
   return (
     <BaseLayout>
       <Card>
-        <div className="m-4 flex h-full items-center gap-10 px-10 pb-5 text-text-700">
+        <div className="m-4 flex gap-12 h-full px-8 items-center py-2 text-text-700">
           <main className="mt-10 w-[40rem]">
             <CardHeader className=" flex items-center">
               <h1 className="text-4xl font-bold">Resultados</h1>
@@ -65,13 +66,14 @@ function OutputLog({ Results, user }) {
             </CardHeader>
             <OutputTable Results={Results} />
           </main>
-          <div className="flex h-full flex-col ">
-            <h1 className="text-5xl font-bold">Estatus de Guardado</h1>
-            <div className="m-4 ">
-              {isPending ? (
-                <h1 className="text-2xl font-bold">Subiendo...</h1>
-              ) : (
-                <div className="max-w-[30rem] overflow-auto break-words text-center">
+          <div className='flex-1 items-center h-full mt-8'>
+              {isPending && isLoading ? (
+                <div>
+                  <h2 className="text-2xl font-bold p-2 text-white bg-gray-500">Subiendo...</h2>
+                </div>
+              ) : !isPending ? (
+                <div className="max-w-[30rem] flex flex-col gap-2 overflow-auto break-words text-center">
+                  <h1 className="text-3xl text-center font-bold">Estatus de Guardado</h1>
                   <p
                     className={clsx('rounded-lg p-3 text-lg text-white', {
                       'bg-danger-500 ': isError,
@@ -80,12 +82,19 @@ function OutputLog({ Results, user }) {
                   >
                     {message}
                   </p>
+                  <div>
+                    <h1 className="text-3xl p-2 text-center font-bold">Estatus de pruebas</h1>
+                    {isPassed && !isPending ? ( // si todas las pruebas pasaron  
+                      <p className="text-lg rounded-md p-2 bg-success-500 text-white">Todas las pruebas pasaron</p>
+                    ) : (
+                      <p className="text-lg rounded-md bg-danger-500 p-2 text-white">Una prueba o varias fallaron</p>
+                    )}
+                  </div>
+                  <div className='mt-2'>
+                  <ExitButton /> 
+                  </div>
                 </div>
-              )}
-            </div>
-          </div>
-          <div className=" mt-auto">
-            <ExitButton />
+              ) : <Spinner color="primary" />}
           </div>
         </div>
       </Card>
--- IRR_electron/src/renderer/src/components/TestComponents/WifiTest/WifiTest.tsx
+++ IRR_electron/src/renderer/src/components/TestComponents/WifiTest/WifiTest.tsx
@@ -1,5 +1,5 @@
 import BaseLayout from '../../ui/baseLayout'
-import { Card, CardBody } from '@nextui-org/react'
+import { Card, CardBody, Button } from '@nextui-org/react'
 // import { useQuery } from '@tanstack/react-query'
 import { useEffect } from 'react'
 import { executeWifiTest } from './services/wifi'
@@ -7,9 +7,29 @@ import { errorData } from '../../../utils/functions'
 import { Spinner } from "@nextui-org/react";
 import { useState } from 'react'
 
+
 function WifiTest({ TestName, nextTest, TestParams }) {
   const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+  const [tries, setTries] = useState(2)
   
+  const retry = () => {
+      setError('')
+      setLoading(true)
+      executeWifiTest(TestParams.config).then((res) => {
+        console.log(res)
+        nextTest(TestName, {
+          result: true,
+          message: "Test de Wifi Exitoso"
+        })
+      }).catch((err) => {
+        setError(errorData(err))
+        setTries(tries - 1)
+      }).finally(() => {
+        setLoading(false)
+      }) 
+  }
+
 
   useEffect(() => {
     setLoading(true)
@@ -21,16 +41,17 @@ function WifiTest({ TestName, nextTest, TestParams }) {
       })
     }).catch((err) => {
       console.log(err)
-
-      nextTest(TestName, {
-        result: false,
-        message: errorData(err)
-      })
-      
+      setError(errorData(err))
     }).finally(() => {
       setLoading(false)
     })
   }, [])
+
+  useEffect(() => {
+    if(tries === 0) {
+      nextTest(TestName, { result: false, message: error })
+    }
+  }, [tries])
 
 
   return (
@@ -41,6 +62,17 @@ function WifiTest({ TestName, nextTest, TestParams }) {
             <div className='flex gap-4 items-center'>
               <p>Ejecutando Prueba de Wifi</p>
               <Spinner color="primary" />
+            </div>
+          )}
+          {error && (
+            <div className='flex flex-col gap-4 items-center'>
+              <p className='text-danger-400 font-bold text-2xl'>Ocurrio un error al ejecutar la prueba de Wifi</p>
+              <div>
+                <p className='text-center text-lg font-bold'>Mensaje de error:</p>
+                <p className='semi-bold text-lg'>{error}</p>
+              </div>
+              <p>Intentos restantes: {tries}</p>
+              <Button color='primary' size="lg" onClick={retry}>Volver a intentar</Button>
             </div>
           )}
         </CardBody>
